1ae2ee88e490dc91fda5ac3609d08118e307772f
DiegoBloise <diego-dev@outlook.com>
2026-04-01 09:54:26 -0300

    feat: enhance UnsortedArray with additional methods for element removal and retrieval

--- Main.java
+++ Main.java
@@ -23,6 +23,46 @@ public class Main {
         System.out.println("\nArray..........: " + unsortedArray.toString());
         System.out.println("Array size.....: " + unsortedArray.size());
         System.out.println("Array capacity.: " + unsortedArray.capacity());
+
+        System.out.println("\n");
+
+        Integer element = 995;
+        System.out.println("Find Element " + element + ": index -> " + unsortedArray.find(element));
+
+        System.out.println("\nFind by index:");
+
+        int idx1 = 5;
+        int idx2 = -1;
+        int idx3 = 2;
+        System.out.println("Invalid index (" + idx1 + ")..: " + unsortedArray.get(idx1));
+        System.out.println("Invalid index (" + idx2 + ").: " + unsortedArray.get(idx2));
+        System.out.println("Valid index (" + idx3 + ")....: " + unsortedArray.get(idx3));
+
+        System.out.println("\nRemoving elements:");
+        System.out.println("Array......: " + unsortedArray.toString());
+        Integer validElement = unsortedArray.get(2);
+        Integer invalidElement = 895;
+
+        unsortedArray.remove(validElement);
+        System.out.println("Remove " + validElement + "...: " + unsortedArray.toString());
+
+        unsortedArray.remove(invalidElement);
+        System.out.println("Remove " + invalidElement + ".: " + unsortedArray.toString());
+
+        System.out.println("\nRemove by index:");
+
+        int invalidIndex1 = unsortedArray.size();
+        int invalidIndex2 = -1;
+        int validIndex = 1;
+
+        unsortedArray.removeAt(invalidIndex1);
+        System.out.println("Invalid index (" + invalidIndex1 + ")..: " + unsortedArray.toString());
+
+        unsortedArray.removeAt(invalidIndex2);
+        System.out.println("Invalid index (" + invalidIndex2 + ").: " + unsortedArray.toString());
+
+        unsortedArray.removeAt(validIndex);
+        System.out.println("Valid index (" + validIndex + ")....: " + unsortedArray.toString());
     }
 
     public static void linkedListTests() {
--- UnsortedArray/UnsortedArray.java
+++ UnsortedArray/UnsortedArray.java
@@ -20,28 +20,81 @@ public class UnsortedArray<T> {
         this.size++;
     }
 
-    public void insert(int index) {
+    public void insert(int index, T element) {
+        if (index >= this.size || index < 0 || element == null) {
+            return;
+        }
 
+        this.array[index] = element;
     }
 
     public void removeAt(int index) {
+        if (index >= this.size || index < 0) {
+            return;
+        }
 
+        this.array[index] = null;
+        this.size--;
+        this.reArrange();
     }
 
     public void remove(T element) {
+        if (element == null) {
+            return;
+        }
 
+        int index = this.find(element);
+
+        if (index < 0) {
+            return;
+        }
+
+        this.array[index] = null;
+        this.size--;
+        this.reArrange();
     }
 
     public void swap(T firstElement, T secondElement) {
+        if (firstElement == null || secondElement == null) {
+            return;
+        }
 
+        int firstElementIndex = this.find(firstElement);
+        int secondElementIndex = this.find(secondElement);
+
+        if (firstElementIndex < 0 || secondElementIndex < 0) {
+            return;
+        }
+
+        T temp = this.array[firstElementIndex];
+        this.array[firstElementIndex] = this.array[secondElementIndex];
+        this.array[secondElementIndex] = temp;
     }
 
     public int find(T element) {
-        return 0;
+        for (int i = 0; i < this.size; i++) {
+            if (this.array[i] == element) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= this.size) {
+            return null;
+        }
+
+        return this.array[index];
     }
 
     private void reArrange() {
-
+        for (int i = 1; i < this.array.length; i++) {
+            if (this.array[i - 1] == null) {
+                this.array[i - 1] = this.array[i];
+            }
+        }
     }
 
     public int size() {
